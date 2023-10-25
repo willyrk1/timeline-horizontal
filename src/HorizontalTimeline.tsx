969d54133da2c59e517a6b3ab8d6226a434a562c
willyrk1 <willyrk1@gmail.com>
2023-10-25 19:50:49 -0400

    Convert to SVG

--- src/HorizontalTimeline.tsx
+++ src/HorizontalTimeline.tsx
@@ -5,9 +5,9 @@ import { getDateNumber } from './utility';
 
 export * from './events';
 
-const topMargin = 50;
+const topMargin = 25;
 
-export interface HorizontalTimelineProps {
+interface HorizontalTimelineProps {
   events: Event[]
   startYear: number
   endYear: number
@@ -15,173 +15,145 @@ export interface HorizontalTimelineProps {
   onBackButton?: () => void
 }
 
-interface HoverCheck {
-  top: number
-  bottom: number
-  left: number
-  right: number
-  detail: string
-}
-
-interface HoverState {
-  x: number
-  y: number
-  detail: string
-}
-
 export default function HorizontalTimeline(props: HorizontalTimelineProps) {
   const { events, startYear, endYear, onForwardButton = () => {}, onBackButton = () => {} } = props
 
-  const canvasRef = React.useRef<HTMLCanvasElement | null>(null);
-
+  interface HoverState {
+    x: number
+    y: number
+    detail: string
+  }
+  
   const [hoverState, setHoverState] = React.useState<HoverState>()
 
-  React.useEffect(() => {
-    const canvas = canvasRef.current
-    if (canvas) {
-      const ctx = canvas.getContext('2d')
-      if (ctx) {
-        canvas.height = window.innerHeight;
-        canvas.width = window.innerWidth;
+  const yearLines: Array<number> = []
+  const interval = endYear - startYear < 30 ? 1 : 10;
+  const startIntervals = Math.ceil(startYear / interval) * interval;
+  const endIntervals = Math.ceil(endYear / interval) * interval;
+  for (let year = startIntervals; year < endIntervals; year += interval) {
+    yearLines.push(year);
+  }
 
-        /////////////////////
-        // Draw year lines //
-        /////////////////////
-        const interval = endYear - startYear < 30 ? 1 : 10;
-        const startIntervals = Math.ceil(startYear / interval) * interval;
-        const endIntervals = Math.ceil(endYear / interval) * interval;
-        for (let year = startIntervals; year < endIntervals; year += interval) {
-          const x = canvas.width * (year - startYear) / (endYear - startYear);
-      
-          ctx.font = "20px serif";
-          const textMetrics = ctx.measureText(year.toString());
-      
-          ctx.save();
-          ctx.beginPath();
-          ctx.globalAlpha = 0.3;
-          ctx.moveTo(x, 10 + textMetrics.actualBoundingBoxAscent);
-          ctx.lineTo(x, canvas.height - 10 - textMetrics.actualBoundingBoxAscent);
-          ctx.stroke();
-          ctx.restore();
-      
-          ctx.fillText(year.toString(), x - textMetrics.width / 2, 5 + textMetrics.actualBoundingBoxAscent);
-          ctx.fillText(year.toString(), x - textMetrics.width / 2, canvas.height - 5);
-        }
+  const backForwardProps = {
+    cy: window.innerHeight / 2,
+    r: 10,
+    fillOpacity: 0,
+    stroke: 'black',
+  }
 
-        //////////////////////////
-        // Forward/back buttons //
-        //////////////////////////
-        const backButton = new Path2D();
-        backButton.arc(20, canvas.height / 2, 10, 0, 2 * Math.PI);
-        ctx.stroke(backButton);
+  let yIndex = 0;
 
-        const forwardButton = new Path2D();
-        forwardButton.arc(canvas.width - 20, canvas.height / 2, 10, 0, 2 * Math.PI);
-        ctx.stroke(forwardButton);
-
-        const handleCanvasClick = ({ x, y }: MouseEvent) => {
-          if (ctx.isPointInPath(backButton, x, y)) {
-            onBackButton();
+  return (
+    <div className='container'>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height={window.innerHeight}
+        width={window.innerWidth}
+        onMouseMove={() => {
+          setHoverState(undefined);
+        }}
+      >
+        {yearLines.map(year => {
+          const x = window.innerWidth * (year - startYear) / (endYear - startYear);
+          const yearLineProps = {
+            x,
+            textAnchor: 'middle',
+            fontFamily: 'serif',
+            fontSize: 20,
           }
-          else if (ctx.isPointInPath(forwardButton, x, y)) {
-            onForwardButton();
+          return (
+            <React.Fragment key={year}>
+              <line
+                x1={x}
+                y1='24'
+                x2={x}
+                y2={window.innerHeight - 22}
+                stroke='black'
+                strokeOpacity='0.3'
+                strokeWidth='1'
+              />
+              <text {...yearLineProps} y='20'>{year}</text>
+              <text {...yearLineProps} y={window.innerHeight - 5}>{year}</text>
+            </React.Fragment>
+          );
+        })}
+
+        <circle {...backForwardProps} onClick={onBackButton} cx='20' />
+        <circle {...backForwardProps} onClick={onForwardButton} cx={window.innerWidth - 20} />
+
+        {events.map(event => {
+          yIndex++;
+          const boxHeight = 25;
+          function hoverProps(detail?: string) {
+            return {
+              onMouseMove: (e: React.MouseEvent) => {
+                setHoverState(undefined);
+                if (detail) {
+                  setHoverState({x: e.clientX, y: e.clientY, detail});
+                }
+                e.stopPropagation();
+              },
+              onMouseLeave: () => {
+                setHoverState(undefined);
+              }
+            }
           }
-        }
-
-        canvas.addEventListener('click', handleCanvasClick);
-
-        ///////////////////////
-        // Historical Events //
-        ///////////////////////
-        const hoverChecks: Array<HoverCheck> = []
-        let yIndex = 0;
-        events.forEach((event) => {
           switch (event.eventType) {
             case 'range': {
               const { title, startDate, endDate, detail } = event;
-            
-              ctx.font = "14px serif";
-              const textMetrics = ctx.measureText(title);
-            
-              const boxLeft = canvas.width * (getDateNumber(startDate) - startYear) / (endYear - startYear);
-              const right = canvas.width * (getDateNumber(endDate) - startYear) / (endYear - startYear);
-            
-              const boxHeight = textMetrics.actualBoundingBoxAscent + 10;
-
-              const top = topMargin + yIndex * (boxHeight + 5);
-              const left = boxLeft - 5 - textMetrics.width;
-            
-              ctx.fillRect(boxLeft, top, right - boxLeft, boxHeight);
-              ctx.fillText(title, left, top + 5 + textMetrics.actualBoundingBoxAscent);
-
-              if (detail) {
-                hoverChecks.push({ top, bottom: top + boxHeight, left, right, detail });
-              }
-              break;
+              const boxLeft = window.innerWidth * (getDateNumber(startDate) - startYear) / (endYear - startYear);
+              const right = window.innerWidth * (getDateNumber(endDate) - startYear) / (endYear - startYear);
+              const y = topMargin + yIndex * boxHeight + 10;
+              return (
+                <g {...hoverProps(detail)} key={title}>
+                  <line
+                    x1={boxLeft}
+                    x2={right}
+                    y1={y}
+                    y2={y}
+                    stroke='black'
+                    strokeWidth='20'
+                  />
+                  <text
+                    x={boxLeft - 5}
+                    y={y + 5}
+                    textAnchor='end'
+                    fontFamily='serif'
+                    fontSize='14'
+                  >
+                    {title}
+                  </text>
+                </g>
+              );
             }
             case 'point': {
               const { date, title, detail } = event;
-            
-              ctx.font = "14px serif";
-              const textMetrics = ctx.measureText(title);
-            
-              const boxHeight = textMetrics.actualBoundingBoxAscent + 10;
-            
-              const top = topMargin + yIndex * (boxHeight + 5)
-
-              const x = canvas.width * (getDateNumber(date) - startYear) / (endYear - startYear);
-              const y = top + boxHeight / 2;
-
-              const left = x - 10 - textMetrics.width;
-            
-              ctx.beginPath();
-              ctx.moveTo(x - 5, y);
-              ctx.lineTo(x, y + 5);
-              ctx.lineTo(x + 5, y);
-              ctx.lineTo(x, y - 5);
-              ctx.fill();
-            
-              ctx.fillText(title, left, top + 5 + textMetrics.actualBoundingBoxAscent);
-
-              if (detail) {
-                hoverChecks.push({ top, bottom: top + boxHeight, left, right: x + 5, detail });
-              }
+              const top = topMargin + yIndex * boxHeight
+              const x = window.innerWidth * (getDateNumber(date) - startYear) / (endYear - startYear);
+              const y = top + boxHeight / 2 - 2.5;
+              return (
+                <g {...hoverProps(detail)} key={title}>
+                  <polygon
+                    points={`${x - 5},${y} ${x},${y + 5} ${x + 5},${y} ${x},${y - 5}`}
+                  />
+                  <text
+                    x={x - 10}
+                    y={y + 5}
+                    textAnchor='end'
+                    fontFamily='serif'
+                    fontSize='14'
+                  >
+                    {title}
+                  </text>
+                </g>
+              )
+            }
+            case 'top':
+              yIndex = 0;
               break;
-            }
           }
-          if (event.eventType === 'top') {
-            yIndex = 0;
-          }
-          else {
-            yIndex++;
-          }
-        });
-
-        ////////////////////
-        // Hover handling //
-        ////////////////////
-        const handleCanvasHover = ({ x, y }: MouseEvent) => {
-          setHoverState(undefined);
-          hoverChecks.forEach(({ top, bottom, left, right, detail }) => {
-            if (x >= left && x <= right && y >= top && y <= bottom) {
-              setHoverState({ x, y, detail });
-            }
-          })
-        }
-
-        window.addEventListener('mousemove', handleCanvasHover)
-
-        return () => {
-          canvas.removeEventListener('click', handleCanvasClick);
-          window.removeEventListener('mousemove', handleCanvasHover);
-        }
-      }
-    }
-  }, [startYear, endYear])
-
-  return (
-    <div className='canvas-container'>
-      <canvas ref={canvasRef} />
+        })}
+      </svg>
       {hoverState && (
         <div style={{ left: hoverState.x + 20, top: hoverState.y + 20 }}>
           {hoverState.detail}
