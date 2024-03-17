4221d6a2238d9f8dfd92bd810334adc1a2f4df7e
willyrk1 <willyrk1@gmail.com>
2024-03-16 21:10:28 -0400

    Many USSR updates

--- src/EndUSSR.tsx
+++ src/EndUSSR.tsx
@@ -47,6 +47,18 @@ export default function EndUSSR() {
           there. Afghan troops continue attacking the Soviet troops even as they
           withdraw.
         `),
+        spacer(),
+        pointEvent("Kádár and others resign (Hungary)", [1988, 5, 22], `
+          János Kádár, head of Hungary's communist party for over 30 years, is pressured
+          to resign along with half a dozen Politburo members as Kádár's health failed
+          and the economy faltered.
+        `),
+        spacer(),
+        pointEvent("JBTZ trial (Slovenia)", [1988, 5, 31], `
+          Four Slovenians are arrested and tried for criticizing the Yugoslavian army
+          and possessing secret military documents. The relatively short sentences
+          serve to only enflame growing tensions and calls for freedom in Slovenia.
+        `),
         top(),
         pointEvent("Soviets finish Afghan withdrawal", [1989, 2, 15], `
           The last Soviet troops leave Afghanistan in defeat. The Afghan communist party
@@ -88,6 +100,16 @@ export default function EndUSSR() {
           unleashes a brutal crackdown with death tolls varying from hundreds to
           tens of thousands.
         `),
+        pointEvent("Nagy reburial (Hungary)", [1989, 6, 16], `
+          Imre Nagy, the executed leader of Hungary's 1956 revolution, is reinterred
+          with full honors after his political rehabilitation. The funeral organized by
+          opponents of the communist government is attended by over 200,000 people.
+        `),
+        rangeEvent("Hungarian Round Table Talks", [1989, 6, 13], [1989, 9, 18], `
+          Hungarian leaders engage in talks modeled on the Polish Round Table Talks
+          as calls for reform increase. The various parties resolve to have free
+          elections and various constitutional reforms.
+        `),
         pointEvent("Polish parliamentary election", [1989, 6, 18], `
           Solidarity wins almost every freely contested election in the first Polish
           election since non-communist approved parties were legalized.
@@ -112,13 +134,26 @@ export default function EndUSSR() {
         `),
         pointEvent("Monday demonstrations begin (East Germany)", [1989, 9, 4], `
           The first in a series of Monday demonstrations (which spread to other days of
-          the week) is held in Leipzig, East Germany. The non-violent protest organized by a prominent local pastor took over city squares and drew the ire of the
+          the week) is held in Leipzig, East Germany. The non-violent protest organized by a prominent local pastor takes over city squares and draws the ire of the
           Stasi secret police. Similar and larger protests spread to other East German
-          cities and contributed to the fall of the Berlin Wall two months later. They continued until March 1990 when free elections took place.
+          cities and contribute to the fall of the Berlin Wall two months later. They continue until March 1990 when free elections take place.
+        `),
+        pointEvent("Slovenia multi-party era", [1989, 9, 27], `
+          Slovenia's Assembly amends the constitution breaking the monopoly on power
+          held by the League of Communists of Slovenia. It also asserts the right to
+          leave Yugoslavia.
+        `),
+        pointEvent("Hungarian one-party rule ends", [1989, 10, 16], `
+          The Marxist-Leninist Hungarian Socialist Workers' Party which ruled
+          Hungary for over 30 years is dissolved and replaced by the social democratic
+          Hungarian Socialist Party. Parliament announces massive constitutional
+          changes including free multi-party elections, separation of powers and
+          guaranteed human and civil rights.
         `),
         pointEvent("Honecher ousted (East Germany)", [1989, 10, 18], `
-          East Germany's dictator since 1971, Erich Honecher, is blamed for all of its
-          recent problems and is voted out by the ruling party, and resigns. In ensuing months, Honecher is arrested and prosecuted, flees to Chile and dies in exile
+          Erich Honecher, East Germany's dictator since 1971, is blamed for all of its
+          recent problems, gets voted out by the ruling party, and resigns. In ensuing months,
+          Honecher is arrested and prosecuted, flees to Chile and dies in exile
           from cancer.
         `),
         pointEvent("Sinatra Doctrine coined", [1989, 10, 25], `
@@ -141,6 +176,28 @@ export default function EndUSSR() {
           toppled as part of the Soviet liberalization and German reunification of the
           late 1980s.
         `),
+        pointEvent("Zhivkov ousted (Bulgaria)", [1989, 11, 10], `
+          In Bulgaria, communist hardliner Todor Zhivkov resigns as the de facto leader of the country.
+          Violent repression of more liberal factions and a disastrous assimilation campaign against
+          Bulgarian Turks
+          causes other government leaders and the Politburo to turn against Zhivkov. When threatened
+          with not only removal but execution, Zhivkov resigns.
+        `),
+        pointEvent("Velvet Revolution begins (Czechoslovakia)", [1989, 11, 17], `
+          Police crack down on a peaceful student demonstration in Prague. While no
+          one is known to have been killed, a story of a fictional student being
+          killed circulates and mobilizes a full revolution.
+        `),
+        pointEvent("Czechoslovakian Presidium resigns", [1989, 11, 24], `
+          A week of strikes and demonstrations drive the entire hardline communist
+          Presidium to resign in Czechoslovakia. They are replaced by more moderate
+          communists days before the communist party is completely removed from power.
+        `),
+        pointEvent("End of Communism in Czechoslovakia", [1989, 11, 28], `
+          The constitution of Czechoslovakia is amended to remove the communist
+          party's leading role in the government. The move effectively ends communism
+          in Czechoslovakia in the second week of the Velvet Revolution.
+        `),
         pointEvent("Malta Summit/end of the Cold War", [1989, 12, 3], `
           American and Soviet leaders Bush and Gorbachev meet in Malta and declare
           the end of the Cold War. It is seen as a major factor in reducing East/West
@@ -151,7 +208,12 @@ export default function EndUSSR() {
           As East German repression, including the Berlin Wall, collapse, government
           and non-government groups initiate "Round Table" talks for the future of
           the country (similar to recent talks in Poland). Initial results are the end
-          of the secret police and the first and only East German free elections.
+          of the secret police and a call for the first and only East German free elections.
+        `),
+        pointEvent("End of one-party rule in Bulgaria", [1989, 12, 11], `
+          In Bulgaria, a month after hardline communist Todor Zhivkov was ousted, with demonstrations
+          sweeping across the country, the new more
+          liberal leader Petar Mladenov announces the end of the Communist Party's monopoly on power.
         `),
         top(),
         pointEvent("Romanian Revolution", [1989, 12, 16], `
@@ -160,11 +222,15 @@ export default function EndUSSR() {
           days later, thousands of civilians are dead from rioting and street fighting,
           Romania's leader is executed and communist rule is ended in Romania.
         `),
-        pointEvent("Romania's Ceaușescu trial and execution", [1989, 12, 25], `
+        pointEvent("Ceaușescu trial and execution", [1989, 12, 25], `
           Romanian Communist Party General Secretary Nicolae Ceaușescu and his wife
           are executed on charges of genocide immediately after a hasty trial. The
           Ceaușescus' deaths bring an end to the Romanian Revolution and to communist
           rule in Romania.
+        `),
+        pointEvent("Havel elected President of Czechoslovakia", [1989, 12, 29], `
+          Poet and Velvet Revolution activist Václav Havel elected President of
+          Czechoslovakia.
         `),
         pointEvent("East German secret police dissolved", [1990, 1, 13], `
           In East Germany, the Office for National Security (formerly Stasi) are
@@ -175,6 +241,12 @@ export default function EndUSSR() {
         pointEvent("Black January (Azerbaijan)", [1990, 1, 20], `
           In Baku, Azerbaijan, Soviet troops violently put down an anti-Soviet movement,
           killing over 100 civilans and injuring hundreds more.
+        `),
+        pointEvent("14th Congress of the LCY", [1990, 1, 22], `
+          The Slovenian and Croatian delegations to the League of Communists of
+          Yugoslavia walk out of the 14th Congress of the LCY after being rejected for
+          democratic reforms and multi-party elections. After the walk-out, the LCY is
+          left effectively powerless.
         `),
         pointEvent("Polish communist party dissolved", [1990, 1, 30], `
           The communist Polish United Workers' Party (PZPR) is dissolved with new
@@ -194,12 +266,29 @@ export default function EndUSSR() {
         pointEvent("East German free election", [1990, 3, 18], `
           East Germany has its first and only free elections. 
         `),
+        pointEvent("Hungarian parliamentary election", [1990, 3, 25], `
+          The first free multi-party election in Hungary since 1945 results in
+          resounding defeat for the former communists.
+        `),
+        pointEvent("Bulgaria abandons Marxism-Leninism", [1990, 4, 3], `
+          Bulgaria's ruling Communist Party, having given up monopoly power the previous winter,
+          formally abandons Marxism-Leninism and rebrands itself the Bulgarian Socialist Party.
+        `),
         pointEvent("Latvia declares transitional independence", [1990, 5, 4], `
           Latvia declares its independence from the Soviet Union stating that the 1940
-          annexation was unconstitutional. The Soviet Union recognized Latvia's
+          annexation was unconstitutional. The Soviet Union recognizes Latvia's
           independence in September 1991.
         `),
-        // top(),
+        pointEvent("Czechoslovak parliamentary election", [1990, 6, 9], `
+          The first parliamentary election in Czechoslovakia since the Communist
+          Party gave up monopoly party results in non-communist majorities in both
+          houses.
+        `),
+        pointEvent("Bulgarian free election", [1990, 6, 10], `
+          Bulgaria holds it's first free multi-party national election in nearly 60 years, six months
+          after the Communist Party relinquished a monopoly on power. The rebranded Bulgarian Socialist
+          Party wins a majority of the Constitutional Assembly seats.
+        `),
         pointEvent("Declaration of State Sovereignty of Armenia", [1990, 8, 23], `
           Armenia declares itself independent from the Soviet Union. Actual
           independence is achieved in September 1991.
@@ -214,9 +303,22 @@ export default function EndUSSR() {
           East Germany dissolves itself by re-establishing its constituent states and
           East Berlin as part of a unified Germany.
         `),
+        pointEvent("Bulgaria renaming and rebranding", [1990, 11, 15], `
+          The People's Republic of Bulgaria's Grand National Assmembly votes to rename the country the
+          Republic of Bulgaria and to remove the Communist state emblem from the flag.
+        `),
+        pointEvent("Bulgarian government collapse", [1990, 12, 7], `
+          Bulgaria's prime minister resigns leading to a new government following large demonstrations
+          and labor strikes.
+        `),
         pointEvent("Wałęsa wins Polish presidency", [1990, 12, 9], `
           In the second round of voting, Solidarity leader wins Poland's first
           presidential election.
+        `),
+        pointEvent("Single-party mandate ends in Albania", [1990, 12, 11], `
+          Following a university student strike, Albania legalizes the formation of new political
+          parties for the first time since 1923. The first multi-party elections happen the following
+          April.
         `),
         pointEvent("Soviet foreign minister resigns", [1990, 12, 20], `
           Soviet foreign minister Eduard Shevardnadze resigns stating that a dictatorship
@@ -233,16 +335,22 @@ export default function EndUSSR() {
           defend strategic objectives. Several civilians are killed in the ensuing
           weeks.
         `),
-        top(),
+        // top(),
         pointEvent("Independence referendum in Lithuania", [1991, 2, 9], `
           Lithuanians approve the first of several independence referendums in the
           dwindling Soviet Union.
+        `),
+        pointEvent("Hoxha statue toppled (Albania)", [1991, 2, 20], `
+          An angry mob of students pull down a statue of Stalinist dictator Enver Hoxha after
+          weeks of demonstration and hunger strikes. The protests began after officials refused to
+          remove Hoxha's name from the university.
         `),
         pointEvent("Warsaw Pact declared disbanded", [1991, 2, 25], `
           Defense and foreign ministers meeting in Hungary declare the Warsaw Pact to be
           disbanded.
         `),
         pointEvent("Independence referendums in Estonia and Latvia", [1991, 3, 3]),
+        top(),
         pointEvent("Soviet Union referendum", [1991, 3, 17], `
           A referendum passes in nine of the fifteen Soviet republics to keep the Soviet
           Union together, but treating the republics as soveriegn entities. The remaining
@@ -250,9 +358,31 @@ export default function EndUSSR() {
           referendum.
         `),
         pointEvent("Independence referendum in Georgia", [1991, 3, 31]),
-        // top(),
+        pointEvent("Socialists win Albanian election", [1991, 3, 31], `
+          Albania has its first multi-party elections since 1923 but the result is a landslide victory
+          for the incumbent socialist party. With the socialists controlling most media, socialist
+          promises for a regulated market economy to stop the rapid deterioration, and rural
+          peasants fearing a return of wealthy land barons, the new anti-communist parties won barely
+          30% of assembly seats.
+        `),
+        pointEvent("Slovenia/Croatia independence", [1991, 6, 25], `
+          Slovenia and Croatia declare independence from Yugoslavia. By that time,
+          Croatia had already been at war for three months vying for independence.
+          Two days after declaring, Slovenia also comes under attack in the Ten-Day
+          War which comes to an end with the Brioni Accord. Croatia continues fighting
+          with Serbia or Serbian-led Yugoslavia for several years.
+        `),
+        pointEvent("Comecon dissolved", [1991, 6, 28], `
+          In Budapest, in its final council session, the Soviet-led Council for Mutual Economic
+          Assistance (Comecon) agrees to dissolve itself effective 90 days later.
+        `),
         pointEvent("Warsaw Pact formally ends", [1991, 7, 1], `
           Czechoslovak President Václav Havel formally disestablishes the Warsaw Treaty.
+        `),
+        pointEvent("Albanian Vlora reaches Italy", [1991, 8, 8], `
+          As post-communist Albania collapses, tens of thousands of desperate refugees storm the
+          cargo ship, Vlora, and set sail for Italy, arriving without warning. Italy reluctantly
+          allows them off the ship but ultimately deport most back to Albania.
         `),
         pointEvent("AUGUST COUP", [1991, 8, 19], `
           Soviet Communist Party hardliners fail in an attempt to overthrow
@@ -297,13 +427,17 @@ export default function EndUSSR() {
           officially independent 2 days later. They had declared themselves
           independent in August 1990.
         `),
+        pointEvent("Socialists lose Bulgarian election", [1991, 10, 13], `
+          In Bulgaria's second free election, socialists and former communists finish a close second.
+        `),
         pointEvent("Azerbaijan declares independence", [1991, 10, 18]),
         pointEvent("Independence referendum in Turkmenistan", [1991, 10, 26]),
         pointEvent("CPSU banished from Russia", [1991, 11, 6], `
           Russian president Boris Yeltsin issues a decree banning the Communist Party of
           the Soviet Union in Russia.
         `),
-        pointEvent("Independence referendums in Ukraine and Transnistria", [1991, 12, 1]),
+        pointEvent("Independence referendum in Ukraine", [1991, 12, 1]),
+        pointEvent("Independence referendum in Transnistria", [1991, 12, 1]),
         pointEvent("Belovezha Accords", [1991, 12, 8], `
           Presidents of Russia, Ukraine and Belarus agree to end the Soviet Union and form
           the Commonwealth of Independent States.
@@ -324,7 +458,19 @@ export default function EndUSSR() {
           The Supreme Soviet of the Soviet Union votes itself out of existence officially
           ending the Soviet Union.
         `),
-        pointEvent("Independence referendums in Azerbaijan and Uzbekistan", [1991, 12, 29]),
+        pointEvent("Independence referendum in Azerbaijan", [1991, 12, 29]),
+        pointEvent("Independence referendum in Uzbekistan", [1991, 12, 29]),
+        top(),
+        pointEvent("Anti-communists win Albanian election", [1992, 3, 22], `
+          In Albania, the last European country to shed communism, the Democratic Party defeats
+          the Socialist Party by wide margins and establish Albania's first non-socialist government
+          since World War II. Socialism would not return to Albania until government and economic
+          scandals in 1997.
+        `),
+        pointEvent("Albania bans Communist Party", [1992, 7, 16], `
+          The Albanian Parliament votes to ban the Communist Party of Albania which had split from
+          the socialist Party of Labour in December 1991.
+        `),
       ]}
     />
   )
